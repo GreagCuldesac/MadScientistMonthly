504a1379f88b80d8c336583d784e86d5fedf7aa0
GreagCuldesac <greag@whocaresproductions.org>
2014-10-23 11:38:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     
     $('div').click(function() {
        if ($(this).is(lightson)){
-          $('div.ledon').filter(lightson).toggleClass('.active');
+          $('div.ledon').filter(lightson).toggleClass('active');
        }
     }); 
 }); 
