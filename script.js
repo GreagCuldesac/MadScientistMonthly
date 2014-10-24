60e7f4499aac5b85c475816a30813f0eafe0e53f
GreagCuldesac <greag@whocaresproductions.org>
2014-10-24 12:38:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         "#bton2",
         "#bton3" 
         ]
-    var allon = 0;
+  
     $("#online").hide();
     var led1 = leds[Math.floor(Math.random()*leds.length)];
     var led2 = 0;
@@ -14,37 +14,27 @@ $(document).ready(function() {
     $('div').click(function() {
        if ($(this).is(led1)){
           $('div.ledon').filter(lightson).toggleClass('active');
-	  allon=allon+1;
-		var led2 = leds[Math.floor(Math.random()*leds.length)];
+	  var led2 = leds[Math.floor(Math.random()*leds.length)];
        } else {
 	  $('.ledon').toggleClass('ledoff');
        }
-	if (allon === 7){
-	 $("#online").show();
-	}
-
+	
     $('div').click(function() {
        if ($(this).is(led2)){
           $('div.ledon').filter(lightson).toggleClass('active');
-	  allon=allon+2;
-		 var led4 = leds[Math.floor(Math.random()*leds.length)];
+	  var led3 = leds[Math.floor(Math.random()*leds.length)];
        } else {
 	  $('.ledon').toggleClass('ledoff');
        }
-	if (allon === 7){
-	 $("#online").show();
-	}
-
+	
     $('div').click(function() {
-       if ($(this).is(led4)){
+       if ($(this).is(led3)){
           $('div.ledon').filter(lightson).toggleClass('active');
-	  allon=allon+4;
+	  $("#online").show();
        } else {
 	  $('.ledon').toggleClass('ledoff');
        }
-	if (allon === 7){
-	 $("#online").show();
-	}
+	
     }); 
    
 }); 
