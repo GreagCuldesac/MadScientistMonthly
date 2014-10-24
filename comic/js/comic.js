c514f92c0a25fa395333e3f2be3342682b706bba
acarroll360 <acarroll@pet360.com>
2014-10-24 11:25:13 -0400

    added fire base and split out js

---
+++ comic/js/comic.js
@@ -0,0 +1,23 @@
+$(document).on('ready', function(){
+	$('.tallyList').hide();
+	var tallyFb = new Firebase("https://intense-inferno-2612.firebaseio.com/"),
+		firstTime = true;
+
+	$('#drbutton').on('click', function(){
+		tallyFb.once('value', function(dataSnapshot) {
+			var tallies = dataSnapshot.val(),
+				keys = Object.keys(tallies),
+				randomKey = Math.floor(Math.random() * keys.length);
+			if (firstTime) {
+				for (var i = 0; i < keys.length; i++) {
+					$('.tallyList').append('<li id=' + i + '>' + keys[i] + ": " + tallies[keys[i]] + '</li>');
+					firstTime = false;
+				};	
+			};
+			tallies[keys[randomKey]]++;
+			$('.tallyList li').eq(randomKey).text(keys[randomKey] + " : " + tallies[keys[randomKey]])
+			tallyFb.set(tallies);
+		});
+		$('.tallyList').show();
+	});    
+});
